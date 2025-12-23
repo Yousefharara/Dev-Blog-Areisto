a842c1e672d83ae432ad076b5f8cce0613c609c3
Yousefharara <hrarty37@gmail.com>
2025-12-23 11:14:49 +0200

    - feat(posts.js) adding constants values

---
+++ src/constants/posts.js
@@ -0,0 +1,10 @@
+export const API_PATHS = {
+  getAllPosts: "posts",
+  getById: "posts/:id",
+};
+
+export const API_ACTION = {
+  IS_LOADING: "IS_LOADING",
+  POSTS: "POSTS",
+  ERROR: "ERROR",
+};
