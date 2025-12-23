0b702391e02b290a242b22c26790a33c4be541f0
Yousefharara <hrarty37@gmail.com>
2025-12-23 11:18:02 +0200

    - feat(header.js): adding content for header with styling

--- src/components/organism/header/index.jsx
+++ src/components/organism/header/index.jsx
@@ -1,8 +1,46 @@
+"use client";
+
 import React from "react";
-import "./style.css";
+import Link from "next/link";
+import Container from "@/components/atoms/container";
+import "./header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { usePostsContext } from "@/context/PostsProvider";
 
 const Header = () => {
-  return <div></div>;
+  const { getAllFavorite } = usePostsContext();
+
+  return (
+    <header>
+      <Container>
+        <nav>
+          <div className="hero">
+            <span>D</span>
+            <p>DevBlog</p>
+          </div>
+          <ul>
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/posts "}>Posts</Link>
+            </li>
+            <li>
+              <Link className="icon" href={"/cart__favorite"}>
+                <FontAwesomeIcon
+                  icon={faHeart}
+                  style={{ color: "var(--primary-clr)" }}
+                  size="lg"
+                />
+                {getAllFavorite() > 0 && <span>{getAllFavorite()}</span>}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
