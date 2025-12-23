c7e1fd1c9e15012ddbfbf66552adebc5816bd10d
Yousefharara <hrarty37@gmail.com>
2025-12-23 11:13:44 +0200

    - no-important

--- src/app/post/[id]/page.jsx
+++
@@ -1,6 +0,0 @@
-import PostDetailsPage from "@/components/pages/postDetails";
-import React from "react";
-
-const PostDetails = (props) => <PostDetailsPage {...props} />;
-
-export default PostDetails;
--- src/app/post/page.jsx
+++
@@ -1,6 +0,0 @@
-import PostsPage from "@/components/pages/postsPage";
-import React from "react";
-
-const Post = (props) => <PostsPage {...props} />;
-
-export default Post;
